74129af78d90fdc1f8ef841d1fa628bfd69f3840
AlexanderCrisler <alexcrisler@gmail.com>
2022-04-22 23:32:15 -0500

    Implement win checking

--- source.js
+++ source.js
@@ -10,7 +10,8 @@ const gameBoard = (() => {
 
     const updateBoard = (location, newValue) => {
         boardVals[location] = newValue;
-        updateDisplay(boardVals);
+        updateDisplay();
+        checkWin();
     }
 
     const makeBlocks = () => {
@@ -25,14 +26,67 @@ const gameBoard = (() => {
         }
     }
     
-    const updateDisplay = (board) => {
+    const updateDisplay = () => {
         for (var i = 0; i < 9; i++) {
-            if (board[i]) {
+            if (boardVals[i]) {
                 let space = document.getElementById(i);
-                space.textContent = board[i];
+                space.textContent = boardVals[i];
             }
         }
     }
+
+    const checkWin = () => {
+        if (
+            boardVals[0] != null &&
+            boardVals[0] == boardVals[1] && 
+            boardVals[0] == boardVals[2]
+        ) {
+            console.log("Winner!", boardVals[0]);
+        } else if (
+            boardVals[3] != null &&
+            boardVals[3] == boardVals[4] &&
+            boardVals[3] == boardVals[5] 
+        ) {
+            console.log("Winner!", boardVals[3]);
+        } else if (
+            boardVals[6] != null &&
+            boardVals[6] == boardVals[7] &&
+            boardVals[6] == boardVals[8]
+        ) {
+            console.log("Winner!", boardVals[6]);
+        } else if (
+            boardVals[0] != null &&
+            boardVals[0] == boardVals[3] &&
+            boardVals[0] == boardVals[6]
+        ) {
+            console.log("Winner!", boardVals[0]);
+        } else if (
+            boardVals[1] != null &&
+            boardVals[1] == boardVals[4] &&
+            boardVals[1] == boardVals[7]
+        ) {
+            console.log("Winner!", boardVals[1]);
+        } else if (
+            boardVals[2] != null &&
+            boardVals[2] == boardVals[5] &&
+            boardVals[2] == boardVals[8]
+        ) {
+            console.log("Winner!", boardVals[2]);
+        } else if (
+            boardVals[0] != null &&
+            boardVals[0] == boardVals[4] &&
+            boardVals[0] == boardVals[8]
+        ) {
+            console.log("Winner!", boardVals[0]);
+        } else if (
+            boardVals[2] != null &&
+            boardVals[2] == boardVals[4] &&
+            boardVals[2] == boardVals[6] 
+        ) {
+            console.log("Winner!", boardVals[2]);
+        }
+    }
+
     return {getBoard, updateBoard, makeBlocks, updateDisplay};
 })();
 
